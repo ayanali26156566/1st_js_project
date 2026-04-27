0eb0f555ceac5e4f8f5cab27097063ea9acb7a11
ayanali261 <ayanalim051@gmail.com>
2026-04-27 23:17:18 +0500

    form

---
+++ sript.js
@@ -0,0 +1,69 @@
+// var table = Number(prompt("Enter the number to print its multiplication table:"));
+// var start= Number(prompt("Enter starting point:"));
+// var end= Number(prompt("Enter ending point:"));
+
+// for (var i = start; i <= end; i++) {
+//     var result = table * i;
+//    document.write(
+//   "<h4 style='text-align:center;'>" +
+//   table + " x " + i + " = " + result +
+//   "</h4><br>"
+// );
+
+
+
+//    console.log(table + " x " + i + " = " + result);
+// }
+
+
+
+
+
+
+//   disible btane wala function
+
+//  function myfunction() {
+    
+
+// let num = +prompt("enter a num") ;
+//  let num2 = +prompt("enter a num") ;
+
+//  let result;
+
+//  if (num % num2 === 0) {
+//     console.log(num, "is divisible by " , num2)
+//  }
+
+//  else{
+
+    
+//    console.log(num,"is not divisible by", num2)
+//  }
+// }
+
+// myfunction()
+
+
+
+//                L------O--------O---------P
+  
+
+
+// localStorage.setItem("username", "Ali");
+// localStorage.setItem("age", "25");
+
+// var username = localStorage.getItem("username");
+// var age = localStorage.getItem("age");
+// console.log("Username: " + username);
+// console.log("Age: " + age);
+
+
+
+function form() {
+    var name = document.getElementById("name").value;
+    var rollno = document.getElementById("rollno").value;
+    var contact = document.getElementById("contact").value;
+    
+}
+ 
+  
